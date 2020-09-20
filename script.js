00bd85a3478f98defd3e258a7fe1556e0dd7aa0b
rzanabusamra22 <rzanabusamra@gmail.com>
2020-09-20 11:58:24 +0300

    uploaded

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [
+{
+ question:"Most popular programming language in 2020 ?", 
+ a:"Javascript",
+ b:"C#" ,
+ c:"Python",
+ d:"Java",
+ correct :"c" 
+}, {
+ question:"What dose HTML stands for ?", 
+ a:"Healthy Teanager Male Language",
+ b:"Home Time Manage Life" ,
+ c:"Happy Text Monday Life",
+ d:"Hyper Text Markup Language",
+ correct :"d" 
+}, {
+ question:"HTML is a programing language ?", 
+ a:"YES",
+ b:"NO" ,
+ c:"Maybe",
+ d:"I do not know",
+ correct :"b" 
+}, {
+ question:"A Syntax Error is ?", 
+ a:"An error you will never find",
+ b:"An error you find at the end of programing" ,
+ c:"An error caused by language rules being broken",
+ d:"An error due to user error",
+ correct :"d" 
+}, {
+ question:"What does VR stands for ?", 
+ a:"Virtual Reality",
+ b:"Virtual Reactions" ,
+ c:"Virtual Realistic",
+ d:"Virtual Roundabouts",
+ correct :"a" 
+}
+]
