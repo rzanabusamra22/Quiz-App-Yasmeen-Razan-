214454d22461681cfa000332c32715f2b0176f58
rzanabusamra22 <rzanabusamra@gmail.com>
2020-09-21 07:39:27 +0300

    updated 7:39am

--- script.js
+++ script.js
@@ -38,59 +38,60 @@ let quizData = [
 }
 ]
 //js Code:
-//calling the Q & A & Button using DOM 
-var questionEl =document.getElementById("question")
-var aText =document.getElementById("a")
-var bText =document.getElementById("b")
-var cText =document.getElementById("c")
-var dText =document.getElementById("d")
+//calling the elements using DOM 
+var questionEl =document.getElementById("question") //for the question 
+var aText =document.getElementById("a") //for the answer a
+var bText =document.getElementById("b") //for the answer b
+var cText =document.getElementById("c") //for the answer c
+var dText =document.getElementById("d") //for the answer d
 
-var answerEls= document.querySelectorAll(".answer")// yasmeen ; this method selects all the elements with the same class
+var answerEls= document.querySelectorAll(".answer") // yasmeen found this method ; it selects all the elements with the same class
 var quiz= document.getElementById("quiz")
 
 var currentQuiz=0;//Current Quiz (index)
-var score=0;
-//load function to laod the Q and A
+var score=0; //to calculate the score 
+
+//it loads the q and a into the quiz everytime 
 loadQuiz();
 function loadQuiz(){
-	unselect()
+	unselect()  // unselect buttons before picking an answer 
 	var currentQuizData = quizData[currentQuiz]; //working
-	questionEl.innerText = currentQuizData.question; //working 
+	questionEl.innerText = currentQuizData.question; // to access the q
 	
-	aText.innerText = currentQuizData.a; 
-	bText.innerText = currentQuizData.b; 
-	cText.innerText = currentQuizData.c;  
-	dText.innerText = currentQuizData.d; 	
+	aText.innerText = currentQuizData.a; // to access the a
+	bText.innerText = currentQuizData.b; // to access the b
+	cText.innerText = currentQuizData.c; // to access the c
+	dText.innerText = currentQuizData.d; // to access the d	
 }
-//To check witch answer is selected.
+//To check what answer is selected and return its id.
 function selected(){
 	var answer = undefined;
     answerEls.forEach(function(answerEl){// using forEach to iterate over each answer with the class answer in answers
-    if(answerEl.checked){
-    	answer=answerEl.id;
+    if(answerEl.checked){ //using the checked prop. for radio button (output:true/false)
+    	answer=answerEl.id; //return the id of the cheked radio button answer.
     }
 });
     return answer;
 }
-function unselect(){
+function unselect(){  // we had a problem when moving to the next question , the radio button is selected before the user do.
     answerEls.forEach(function(answerEl){
-    answerEl.checked=false;    
+    answerEl.checked=false;  // to unckeck , using the unchecked prop. of radio button.   
 });
 }
 
 //Using jQuery on click function
 $('#subButton').on('click', function(e){ 
-	var answer= selected()
-	if(answer){ 
-	if(answer === quizData[currentQuiz].correct){
-		score++;
+	var answer= selected()    //after selecting an answer 
+	if(answer){               //to check if it is selected 
+	if(answer === quizData[currentQuiz].correct){  // the answer is equal to the right answer in the array of obj !
+		score++; // add1 to the score 
 	}
-     currentQuiz++;
-     if(currentQuiz < quizData.length){
+     currentQuiz++;  //move to the next q.
+     if(currentQuiz < quizData.length){ //did not reach the finale question keep loading the other ones. 
 	  loadQuiz();
 	  }
-	  else{
-	 	quiz.innerHTML ="<h2>You answered correctly at  $(score) / $(quizData.length) questions.</h2>"
+	  else{ //to display the score.
+	 	quiz.innerHTML ="<h2>You answered correctly at  $(score) / $(quizData.length) questions.</h2>"  
 	 	// how to show the score ??
 	 	// select only one !!
 	 	//documentation. 
